997164d282d829bd8e947ab1de9695c64336a11a
Matteo Giachino <matteog@gmail.com>
2015-02-07 22:45:06 +0100

    php 5.3 compatibility

--- tests/Cypress/Curry/functions_test.php
+++ tests/Cypress/Curry/functions_test.php
@@ -15,70 +15,70 @@ class functionsTest extends \PHPUnit_Framework_TestCase
 
     public function test_curry_identity()
     {
-        $identity = C\curry([new TestSubject(), 'identity'], 1);
+        $identity = C\curry(array(new TestSubject(), 'identity'), 1);
         $this->assertEquals(1, $identity(1));
     }
 
     public function test_curry_with_one_later_param()
     {
-        $curriedOne = C\curry([new TestSubject(), 'add2'], 1);
+        $curriedOne = C\curry(array(new TestSubject(), 'add2'), 1);
         $this->assertInstanceOf('Closure', $curriedOne);
         $this->assertEquals(2, $curriedOne(1));
     }
 
     public function test_curry_with_two_later_param()
     {
-        $curriedTwo = C\curry([new TestSubject(), 'add4'], 1, 1);
+        $curriedTwo = C\curry(array(new TestSubject(), 'add4'), 1, 1);
         $this->assertInstanceOf('Closure', $curriedTwo);
         $this->assertEquals(4, $curriedTwo(1, 1));
     }
 
     public function test_curry_with_successive_calls()
     {
-        $curriedTwo = C\curry([new TestSubject(), 'add4'], 1, 1);
+        $curriedTwo = C\curry(array(new TestSubject(), 'add4'), 1, 1);
         $curriedThree = $curriedTwo(1);
         $this->assertEquals(4, $curriedThree(1));
     }
 
     public function test_curry_right()
     {
-        $divideBy10 = C\curry_right([new TestSubject(), 'divide2'], 10);
+        $divideBy10 = C\curry_right(array(new TestSubject(), 'divide2'), 10);
         $this->assertInstanceOf('Closure', $divideBy10);
         $this->assertEquals(10, $divideBy10(100));
     }
 
     public function test_curry_right_immediate()
     {
-        $divide3 = C\curry_right([new TestSubject(), 'divide3'], 5, 2, 20);
+        $divide3 = C\curry_right(array(new TestSubject(), 'divide3'), 5, 2, 20);
         $this->assertEquals(2, $divide3());
     }
 
     public function test_curry_left_immediate()
     {
-        $divide3 = C\curry([new TestSubject(), 'divide3'], 20, 2, 4);
+        $divide3 = C\curry(array(new TestSubject(), 'divide3'), 20, 2, 4);
         $this->assertEquals(2.5, $divide3());
     }
 
     public function test_curry_three_times()
     {
-        $divideBy5 = C\curry([new TestSubject(), 'divide3'], 100);
+        $divideBy5 = C\curry(array(new TestSubject(), 'divide3'), 100);
         $divideBy10And5 = $divideBy5(10);
         $this->assertEquals(2, $divideBy10And5(5));
     }
 
     public function test_curry_right_three_times()
     {
-        $divideBy5 = C\curry_right([new TestSubject(), 'divide3'], 5);
+        $divideBy5 = C\curry_right(array(new TestSubject(), 'divide3'), 5);
         $divideBy10And5 = $divideBy5(10);
         $this->assertEquals(2, $divideBy10And5(100));
     }
 
     public function test_rest()
     {
-        $this->assertEquals([1], C\_rest([1, 1]));
-        $this->assertEquals(['a', 'b'], C\_rest([1, 'a', 'b']));
-        $this->assertEquals([], C\_rest([1]));
-        $this->assertEquals([], C\_rest([]));
+        $this->assertEquals(array(1), C\_rest(array(1, 1)));
+        $this->assertEquals(array('a', 'b'), C\_rest(array(1, 'a', 'b')));
+        $this->assertEquals(array(), C\_rest(array(1)));
+        $this->assertEquals(array(), C\_rest(array()));
     }
 
     /**
@@ -91,15 +91,15 @@ class functionsTest extends \PHPUnit_Framework_TestCase
 
     public function provider_is_fullfilled()
     {
-        return [
-            [false, [], function ($a) {}],
-            [true, [], function () {}],
-            [true, [1], function ($a) {}],
-            [false, [1], function ($a, $b) {}],
-            [false, [1], [new TestSubject(), 'add2']],
-            [true, [1, 2], [new TestSubject(), 'add2']],
-            [true, ['aaa', 'a'], 'strpos'],
-        ];
+        return array(
+            array(false, array(), function ($a) {}),
+            array(true, array(), function () {}),
+            array(true, array(1), function ($a) {}),
+            array(false, array(1), function ($a, $b) {}),
+            array(false, array(1), array(new TestSubject(), 'add2')),
+            array(true, array(1, 2), array(new TestSubject(), 'add2')),
+            array(true, array('aaa', 'a'), 'strpos'),
+        );
     }
 }
 
